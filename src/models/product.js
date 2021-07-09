570385b8f06677f5cfd4f92b2b9c17ad9d9b3fa6
Donisw <migueldonis@outlook.com>
2021-07-09 01:28:05 -0400

    modelo de producto

--- src/models/product.js
+++ src/models/product.js
@@ -1,7 +1,10 @@
 const {Schema, model} = require("mongoose");
 
 const productSchema = new Schema ({
-
+    nombre:{type: string, required: true},
+    color:{type: string, required: true},
+    modelo:{type: string, required: true},
+    date:{type: Date, default: Date.now}
 })
 
 module.exports = model("Product", productSchema)
